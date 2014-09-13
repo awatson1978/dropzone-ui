dcc007bdead0ea00f22baa2ffe345e47b363ff4a
Abigail Watson <awatson1978@gmail.com>
2014-09-13 15:50:56 -0400

    dropzone updated to 0.9.1

--- dropzone-test.js
+++ dropzone-test.js
@@ -2,53 +2,54 @@ if (Meteor.isClient) {
   Template.hello.greeting = function () {
     return "Welcome to dropzone-test.";
   };
+
+  Router.configure({
+  	layoutTemplate: 'appLayout'
+  });
+
+  Router.map(function () {
+    this.route('hello', {
+      path: '/',
+      template: 'hello'
+    });
+  });
 }
 
 if (Meteor.isServer) {
 
   Router.map(function () {
-    this.route('upload-creatives', {
+    this.route('uploads', {
       where: 'server',
       action: function () {
+        console.log('/upload triggered... ');
+
         var fs = Npm.require('fs');
         var path = Npm.require('path');
         var self = this;
+
+        ROOT_APP_PATH = fs.realpathSync('.');
+        console.log('ROOT_APP_PATH: ', ROOT_APP_PATH);
+        console.log('this.request.files.file.path', this.request.files.file.path);
+
 
         // dropzone.js stores the uploaded file in the /tmp directory
         // we access the /tmp directory
         fs.readFile(self.request.files.file.path, function (err, data) {
 
           // and then write the file to the uploads directory
-          fs.writeFile('/uploads/' + self.request.files.file.name, data, function (error) {
+          fs.writeFile(ROOT_APP_PATH + "/assets/app/uploads/" +self.request.files.file.name, data, 'binary', function (error, result) {
             if(error){
               console.error(error);
             }
-          });
-        });
-      }
-    });
-
-    this.route('upload-products', {
-      where: 'server',
-      action: function () {
-        var fs = Npm.require('fs');
-        var path = Npm.require('path');
-        var self = this;
-
-        // dropzone.js stores the uploaded file in the /tmp directory
-        // we access the /tmp directory
-        fs.readFile(self.request.files.file.path, function (err, data) {
-
-          // and then write the file to the uploads directory
-          fs.writeFile(self.request.files.file.name, data, function (error) {
-            if(error){
-              console.error(error);
+            if(result){
+              console.log('Success! ', result);
             }
           });
         });
+
+
       }
     });
-
   });
 
 }
